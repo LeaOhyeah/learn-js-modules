699eb9a015ad69c8d584e6b41ba9f095f1236f6f
LeaOhyeah <leasehat@gmail.com>
2025-06-08 10:04:19 +0800

    day 2 - default and named, module object

---
+++ scripts/module.js
@@ -0,0 +1,14 @@
+export const firstName = "Lea";
+export const middleName = "Alyu";
+export const lastName = "Maulana";
+
+export function hello(){
+    console.info("Hello from Lea");
+}
+
+export class Person {
+
+    constructor() {
+        this.name = "Lea";
+    }
+}
